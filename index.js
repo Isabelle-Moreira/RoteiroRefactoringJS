34536c17fb6f3dd8a50e09ccd617e96d7f6c6631
Isabelle <isabelleyasmor@gmail.com>
2024-09-11 19:47:31 -0300

    Replace Temp with Query

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const { readFileSync } = require('fs');
 
+
+ // função query
+ function getPeca(apresentacao) {
+  return pecas[apresentacao.id];
+}
+
 function gerarFaturaStr (fatura, pecas) {
+
     let totalFatura = 0;
     let creditos = 0;
     let faturaStr = `Fatura ${fatura.cliente}\n`;
@@ -33,17 +40,17 @@ function gerarFaturaStr (fatura, pecas) {
 
   
     for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
+      //const peca = pecas[apre.id];
       
-      let total = calcularTotalApresentacao(apre, peca);
+      let total = calcularTotalApresentacao(apre, getPeca(apre));
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
